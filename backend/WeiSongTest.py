eb0b0ad99016cc9bcd7096a5137c98a6d8b5b0f3
宋伟 <46225024+ShermanIsCoding@users.noreply.github.com>
2019-11-09 12:23:08 +1100

    data preparing  FINISH

--- backend/WeiSongTest.py
+++ backend/WeiSongTest.py
@@ -1,78 +1,31 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler
 
-def mapPropertyType(propertyType):
-    """this function is used to handle the property_tyoe column"""
+def normalization(dfDataset):
+    """this function is used to do normalization for the dataset"""
 
-    if propertyType == 'Apartment':
-        return 0
-    elif propertyType == 'House':
-        return 1
+    for col in dfDataset.columns:
+        MIN = min(dfDataset[col])
+        MAX = max(dfDataset[col])
+        newCol = dfDataset[col].apply(lambda x: (x - MIN) / (MAX - MIN))
+        dfDataset = dfDataset.drop(col, axis=1)
+        dfDataset[col] = newCol
 
-def mapRoomType(roomType):
-    """this function is used to handle the room type column"""
-
-    if roomType == 'Entire home/apt':
-        return 0
-    elif roomType == 'Private room':
-        return 1
-    elif roomType == 'Shared room':
-        return 2
-
-def mapBedType(bedType):
-    """this function is used to handle the bed type column"""
-
-    if bedType == 'Real Bed':
-        return 0
-    elif bedType == 'Futon':
-        return 1
-    elif bedType == 'Airbed':
-        return 2
-    elif bedType == 'Pull-out Sofa':
-        return 3
-
-def mapCancellationPolicy(policy):
-    if policy == 'strict':
-        return 3
-    elif policy == 'moderate':
-        return 2
-    elif policy == 'flexible':
-        return 1
-
-def mapCleaningFee(value):
-    if value:
-        return 1
-    else:
-        return 0
-
-def mapHostHasProfilePic(value):
-    if value == 't':
-        return 1
-    else:
-        return 0
-
-def mapHostIdentityVerified(value):
-    if value == 't':
-        return 1
-    else:
-        return 0
-
-def mapInstanceBookable(value):
-    if value == 't':
-        return 1
-    else:
-        return 0
+    return dfDataset
 
 def preProcessDataSet():
+    """this function is uesed to pre process the dataset for modelling"""
 
     dataSetTrain = 'dataset.csv'
     dfDataset = pd.read_csv(dataSetTrain)
-
     # these columns are hard to use for KNN, so I just drop provisonally
     columnNeedDrop = [
-        'latitude', 'longitude', 'thumbnail_url', 'neighbourhood'
+        'latitude', 'longitude', 'thumbnail_url', 'neighbourhood', 'host_has_profile_pic',
+        'host_identity_verified', 'zipcode', 'host_since', 'id', 'last_review', 'first_review',
+        'city', 'description', 'name', 'amenities'
     ]
+
     dfDataset.drop(columnNeedDrop, axis=1, inplace=True)
     # map property type as number, the rule is {Apartment: 0.0, House: 1.0}
     dfDataset['property_type'] = LabelEncoder().fit_transform(dfDataset['property_type'])
@@ -85,25 +38,39 @@ def preProcessDataSet():
     # map cleaning_fee
     dfDataset['cleaning_fee'] = LabelEncoder().fit_transform(dfDataset['cleaning_fee'])
     # map host_has_profile_pic
-    dfDataset['host_has_profile_pic'] = dfDataset['host_has_profile_pic'].apply(mapHostHasProfilePic)
-    # map host_identity_verified
-    dfDataset['host_identity_verified'] = dfDataset['host_identity_verified'].apply(mapHostIdentityVerified)
+    # dfDataset['host_has_profile_pic'] = dfDataset['host_has_profile_pic'].apply(mapHostHasProfilePic)
+    # # map host_identity_verified
+    # dfDataset['host_identity_verified'] = dfDataset['host_identity_verified'].apply(mapHostIdentityVerified)
     # map instant_bookable
     dfDataset['instant_bookable'] = LabelEncoder().fit_transform(dfDataset['instant_bookable'])
-    cityLabel = LabelEncoder().fit_transform(dfDataset['city'])
-    dfDataset['city'] = OneHotEncoder(categories='auto').fit_transform(cityLabel.reshape(-1, 1))
-    amenitiesLabel = LabelEncoder().fit_transform(dfDataset['amenities'])
-    dfDataset['amenities'] = OneHotEncoder(categories='auto').fit_transform(amenitiesLabel.reshape(-1, 1))
-    nameLabel = LabelEncoder().fit_transform(dfDataset['name'])
-    dfDataset['name'] = OneHotEncoder(categories='auto').fit_transform(nameLabel.reshape(-1, 1))
-    descriptionLabel = LabelEncoder().fit_transform(dfDataset['description'])
-    dfDataset['description'] = OneHotEncoder(categories='auto').fit_transform(descriptionLabel.reshape(-1, 1))
 
+    # cityLabel = LabelEncoder().fit_transform(dfDataset['city'])
+    # dfDataset['city'] = OneHotEncoder(categories='auto').fit_transform(cityLabel.reshape(-1, 1))
+    #
+    # amenitiesLabel = LabelEncoder().fit_transform(dfDataset['amenities'])
+    # dfDataset['amenities'] = OneHotEncoder(categories='auto').fit_transform(amenitiesLabel.reshape(-1, 1))
+    #
+    # nameLabel = LabelEncoder().fit_transform(dfDataset['name'])
+    # dfDataset['name'] = OneHotEncoder(categories='auto').fit_transform(nameLabel.reshape(-1, 1))
+    #
+    # descriptionLabel = LabelEncoder().fit_transform(dfDataset['description'])
+    # dfDataset['description'] = OneHotEncoder(categories='auto').fit_transform(descriptionLabel.reshape(-1, 1))
+
+    print('before dropna')
     print(dfDataset.shape[1], dfDataset.shape[0])
     dfDataset.dropna(axis=0, inplace=True)
+    print('after dropna')
     print(dfDataset.shape[1], dfDataset.shape[0])
-    print(dfDataset.head(10))
+    print(dfDataset.dtypes)
 
+    dfDataset['host_response_rate'] = dfDataset['host_response_rate'].str.replace('%', '').astype(float)
+
+    # finally, you get the data you can use for modelling
+    dfDataset = normalization(dfDataset)
+    # we use review_scores_rating as y
+    allLabelData = dfDataset['review_scores_rating']
+    # and left are our features
+    allFeatureData = dfDataset.drop('review_scores_rating', axis=1)
 
 def main():
     preProcessDataSet()
