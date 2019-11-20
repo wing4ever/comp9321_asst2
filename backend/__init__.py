6c656ba670c81bd680807ce55a5b0465c768cf26
Gauraw Rijal <g.rijal@neura.edu.au>
2019-11-21 00:30:42 +1100

    Setup swagger doc, move login and signup to api

--- backend/__init__.py
+++ backend/__init__.py
@@ -27,13 +27,11 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint)
-
     return app
+
