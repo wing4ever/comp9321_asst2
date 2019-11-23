e0b1cb686d75b2411e8623556b27a4a9ef6490a6
Gauraw Rijal <g.rijal@neura.edu.au>
2019-11-23 17:57:11 +1100

    Pluralize table name and create 2 users ‘admin’ and ‘user’

--- backend/model.py
+++ backend/model.py
@@ -7,6 +7,7 @@ import json
 
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100), unique=True)
